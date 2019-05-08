7c381c4634eda1f775bff608208f5e74517f45c2
BehindLoader <simonsuprun@gmail.com>
2019-05-09 00:01:21 +0300

    Add customizing with environment variables

--- server/server/settings.py
+++ server/server/settings.py
@@ -4,10 +4,13 @@
 import os
 
 
+# Hardcoded settings
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = 'SECRET_KEY'
-DEBUG = True
-ALLOWED_HOSTS = []
 INSTALLED_APPS = []
 ROOT_URLCONF = 'server.urls'
-TIME_ZONE = 'UTC'
+
+# Customizable settings
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
+DEBUG = os.getenv('DEBUG') in ['True', 'true', '1']
+SECRET_KEY = os.getenv('SECRET_KEY')
+TIME_ZONE = os.getenv('TIME_ZONE', 'UTC')
