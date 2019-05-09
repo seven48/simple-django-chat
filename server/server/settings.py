071f391a03f876a52ceb59b86eb0205c88949d0c
BehindLoader <simonsuprun@gmail.com>
2019-05-10 01:53:24 +0300

    Migrate from MongoDB to PostgreSQL

--- server/server/settings.py
+++ server/server/settings.py
@@ -6,21 +6,24 @@ import os
 
 # Hardcoded settings
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-INSTALLED_APPS = []
-DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': os.getenv('MONGODB_NAME', 'gachichat'),
-        'HOST': os.getenv('MONGODB_HOST', 'localhost'),
-        'PORT': int(os.getenv('MONGODB_PORT', '27017')),
-        'USER': os.getenv('MONGODB_USER'),
-        'PASSWORD': os.getenv('MONGODB_PASSWORD')
-    }
-}
+INSTALLED_APPS = [
+    'users'
+]
 ROOT_URLCONF = 'server.urls'
 
 # Customizable settings
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_NAME', 'gachichat'),
+        'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': int(os.getenv('POSTGRES_PORT', '5432')),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD')
+    }
+}
 DEBUG = os.getenv('DEBUG') in ['True', 'true', '1']
 SECRET_KEY = os.getenv('SECRET_KEY')
 TIME_ZONE = os.getenv('TIME_ZONE', 'UTC')
+USER_TOKEN_EXPIRING = int(os.getenv('USER_TOKEN_EXPIRING', '604800'))
