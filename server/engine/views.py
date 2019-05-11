f5f3a930eaeecbd2631b5d5a6c7444e0b833de02
BehindLoader <simonsuprun@gmail.com>
2019-05-11 03:14:43 +0300

    Add application/json data parser preloader

--- server/engine/views.py
+++ server/engine/views.py
@@ -1,18 +1,48 @@
 """ Abstract views with error handlers etc """
 
 
+import json
+
+from datetime import datetime
+
 from django.http import JsonResponse
 
 from server.settings import DEBUG
 
 
-class WrongMethod(Exception):
-    """ Request method is not allowed """
-    pass
-
-
 class ABSView:
     """" Abstract view class """
+
+    def __new__(cls, request, methods=None):
+        """ Overriding class creating """
+        inst = super(ABSView, cls).__new__(cls)
+        inst.request = request
+        inst.methods = methods
+
+        if methods:
+            if request.method not in methods:
+                err = Exception(
+                    'Method {} is not allowed'.format(
+                        request.method
+                    )
+                )
+                return inst._error(err)
+
+        return inst._handler()
+
+    def _handler(self):
+        """ Error catcher """
+        try:
+            return self.response()
+        except Exception as err:
+            return self._error(err)
+
+    def response(self):
+        """ Build response data """
+        return JsonResponse({
+            'status': 'ok',
+            'data': self.data()
+        })
 
     @staticmethod
     def data():
@@ -36,6 +66,7 @@ class ABSView:
 
     def _error(self, error):
         """ Private error handling """
+        self.error(error)
         response = {}
         response['status'] = 'error'
         response['error'] = {}
@@ -53,26 +84,32 @@ class ABSView:
 
 
 class View(ABSView):
-    def __new__(cls, request, methods=None):
-        """ Overriding class creating """
-        inst = super(View, cls).__new__(cls)
-        inst.request = request
-        inst.methods = methods
+    def get_JSON_body(self):
+        if self.request.content_type == 'application/json':
+            if self.request.body:
+                return json.loads(self.request.body)
+        return {}
 
-        if methods:
-            if request.method not in methods:
-                err = WrongMethod(
-                    'Method {} is not allowed'.format(
-                        request.method
-                    )
-                )
-                return inst._error(err)
-
+    def _handler(self):
         try:
-            return JsonResponse({
-                'status': 'success',
-                'data': inst.data()
-            })
+            self.request.json = self.get_JSON_body()
+            return self.response()
+        except json.JSONDecodeError:
+            return self._error(Exception('JSON data is not valid'))
         except Exception as err:
-            inst.error(err)
-            return inst._error(err)
+            return self._error(err)
+
+
+class LongPollingView(ABSView):
+    def response(self):
+        start = int(datetime.now().timestamp())
+
+        while True:
+            now = int(datetime.now().timestamp())
+
+            data = self.data()
+            if data:
+                return JsonResponse(data)
+
+            if now > start + 25:
+                return JsonResponse([])
--- server/rooms/views/create.py
+++ server/rooms/views/create.py
@@ -1,14 +1,7 @@
-import json
-
 from engine.views import View
 from rooms.models import Room
 
 
 class Route(View):
     def data(self):
-        try:
-            body = json.loads(self.request.body)
-        except json.JSONDecodeError:
-            return Room.objects.new_room()
-        else:
-            return Room.objects.new_room(**body)
+        return Room.objects.new_room(**self.request.json)
